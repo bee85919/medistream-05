178c32fedb44bca4a6d3965d96805ba7df69c59f
bee <bee85919@gmail.com>
2023-12-17 23:54:33 +0900

    Update merge-manager.py

--- pyspark/merge-manager.py
+++ pyspark/merge-manager.py
@@ -11,6 +11,7 @@ csvs_path = f"{root_path}/pyspark/naverplace"
 save_path = f"{root_path}/result"
 
 ###
+# csv(csvs_path + "/*.csv") => csvs_path의 csv확장자명의 모든 csv 파일을 읽을 수 있다.
 df = spark.read.option("header", "true").option("encoding", "UTF-8").csv(csvs_path + "/*.csv")
 df = df.dropDuplicates()
 df.coalesce(1).write.option("header", "true").option("encoding", "UTF-8").csv(save_path)
