24543a745fa659a201f8e32e713d81b7b67d837c
bee <bee85919@gmail.com>
2023-12-17 23:54:35 +0900

    Update run.sh

--- pyspark/run.sh
+++ pyspark/run.sh
@@ -4,17 +4,19 @@ QUEUEMANAGER_PATH="./queue-manager.py"
 PREPROCESSOR_PATH="./medi05-pyspark-preprocessor.py"
 MERGEMANAGER_PATH="./merge-manager.py"
 
-### create a queue text file
+### create queue text file
 python $QUEUEMANAGER_PATH
 
-### unset envs
+### unset envs (local jupyter lab 환경 변수와 충돌 발생하기 때문에 unset)
 unset PYSPARK_DRIVER_PYTHON
 unset PYSPARK_DRIVER_PYTHON_OPTS
 
 ### spark-submit
 count=0
+# queue text file이 있다면 while문이 실행
 while [ -f $FILE_PATH ]
 do
+    # 공백을 제외하고 다른 값이 없으면 queue가 없다고 판단함
     if ! cat $FILE_PATH | grep -q '[^[:space:]]'; then
         echo "a queue text file is empty."
         break
